ac43652e900b12a2c7d68b292799bc9d8eac0209
I m r a n <imranulm@zeta.tech>
2023-05-14 22:50:31 +0530

    first push

---
+++ README.md
@@ -0,0 +1,5 @@
+# AI powered blog writer
+
+## Description
+This is a blog writer that uses AI to generate content for your blog. It uses the GPT-3 model (soon to be ported to GPT 4 once the API access is made available) to generate content. The model is trained on a huge dataset and it covers almost all the topics that you might need to write about. Currently limiting the api calls (obviously as I dnt have unlimited budget for api usage) but if you want to use it for your own blog, you can purchase the api key from openai and I 'll host it in a separate server with your own api key.
+ 
---
+++ pages/api/apiCall.js
@@ -0,0 +1,53 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "API key issue",
+      }
+    });
+    return;
+  }
+
+  const { topic, focusKeyword, length, targetAudience } = req.body;
+  if (!topic || !focusKeyword || !length || !targetAudience) {
+    res.status(400).json({
+      error: {
+        message: "Please provide all required fields",
+      },
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(topic, focusKeyword, length, targetAudience),
+      temperature: 0.6,
+      max_tokens: 2048,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(topic, focusKeyword, length, targetAudience) {
+  return `Write a blog post about the topic "${topic}" using the focus keyword "${focusKeyword}" for a ${targetAudience} audience. The blog post should be approximately ${length} words long. Also add a meta description for the blog post:`;
+}
