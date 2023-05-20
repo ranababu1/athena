dfe064b4aa65876ee14d6ff6cb896bf5e2efe1b6
I m r a n <imranulm@zeta.tech>
2023-05-20 19:07:16 +0530

    fixed component error

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/global.css";
 import { useState, useRef } from "react";
 import jsPDF from "jspdf";
 import "bootstrap/dist/css/bootstrap.min.css";
-import HeroSection from "../components/HeroSection";
 import ContentForm from "../components/ContentForm";
 import Herosection from "../components/Herosection";
 
